2d54cb16d9cd4e34eb542d7043ca6bd7339b0317
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-04-22 23:28:40 -0400

    added DB connection using pg module

--- twitter-clone-app/scripts/load-fake-data.ts
+++ twitter-clone-app/scripts/load-fake-data.ts
@@ -1,4 +1,28 @@
-function loadFakeData(numUsers: number =10){
-    console.log(`executing load fake data. Generating ${numUsers} users`);
+import { Client } from "pg";
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+async function loadFakeData(numUsers = 10) {
+    console.log(`Executing load fake data. Generating ${numUsers} users`);
+
+    const client = new Client({
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_NAME,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT!)
+    });
+
+    try {
+        await client.connect();
+        const res = await client.query("SELECT 1"); 
+        console.log("Query result:", res.rows);
+    } catch (error) {
+        console.error("Error executing query:", error);
+    }
+    await client.end()
 }
-loadFakeData()
+
+loadFakeData();
--- twitter-clone-app/sql/1u.sql
+++ twitter-clone-app/sql/1u.sql
@@ -1,34 +1,31 @@
-CREATE EXTENSION IF NOT EXISTS citext;
+create extension if not exists citext;
 
--- Creating the users table
-CREATE TABLE IF NOT EXISTS public.users (
-    id SERIAL PRIMARY KEY,
-    username CITEXT UNIQUE NOT NULL,
-    password TEXT, 
-    avatar TEXT,
-    is_admin BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+create table if not exists public.users (
+  id bigserial primary key,
+  username citext unique not null,
+  password text,
+  avatar text,
+  is_admin boolean default false,
+  created_at timestamp default now(),
+  updated_at timestamp default now()
 );
 
--- Creating the follows table
-CREATE TABLE IF NOT EXISTS public.follows (
-    user_id BIGINT NOT NULL REFERENCES public.users(id),
-    follower_id BIGINT NOT NULL REFERENCES public.users(id),
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+create table if not exists public.posts (
+  id bigserial primary key,
+  user_id bigint references public.users (id),
+  content text,
+  created_at timestamp default now(),
+  updated_at timestamp default now()
 );
 
--- Creating the posts table
-CREATE TABLE IF NOT EXISTS public.posts (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT REFERENCES public.users(id),
-    content TEXT,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
+create table if not exists public.follows (
+  user_id bigint not null references public.users (id),
+  follower_id bigint not null references public.users (id),
+  created_at timestamp default now(),
+  updated_at timestamp default now(),
+  unique(user_id, follower_id)
 );
 
-
-CREATE INDEX IF NOT EXISTS posts_user_id_index ON public.posts(user_id);
-CREATE INDEX IF NOT EXISTS follows_user_id_index ON public.follows(user_id);
-CREATE INDEX IF NOT EXISTS follows_follower_id_index ON public.follows(follower_id);
+create index posts_user_id_index on public.posts (user_id);
+create index follows_user_id_index on public.follows (user_id);
+create index follows_follower_id_index on public.follows (follower_id);
