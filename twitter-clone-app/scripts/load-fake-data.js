00920b00566818e13e912d07d2b43e0dce6a53dc
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-04-24 12:29:44 -0400

    added load admin user and refactoring DB connection

--- twitter-clone-app/scripts/load-fake-data.js
+++
@@ -1,5 +0,0 @@
-function loadFakeData(numUsers) {
-    if (numUsers === void 0) { numUsers = 10; }
-    console.log("executing load fake data. Generating ".concat(numUsers, " users"));
-}
-loadFakeData();
--- twitter-clone-app/scripts/load-fake-data.ts
+++ twitter-clone-app/scripts/load-fake-data.ts
@@ -1,26 +1,20 @@
-import { Client } from "pg";
-import { loadEnvConfig } from "@next/env";
-import {faker} from "@faker-js/faker"
+import {faker} from "@faker-js/faker";
+import bcrypt from "bcrypt";
+import { getClient } from "@/db";
 
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
 
 async function loadFakeData(numUsers = 10) {
     console.log(`Executing load fake data. Generating ${numUsers} users`);
 
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_NAME,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!)
-    });
+    const client = await getClient();
     await client.connect();
     try {
         await client.query("begin")
 
         for (let i = 0; i < numUsers; i++){
-            await client.query("insert into public.users (username, password, avatar) values($1, $2, $3)", [faker.internet.userName(), "password", faker.image.avatar()])
+            const saltRounds = 10;
+            const hash = await bcrypt.hash("strings123", saltRounds);
+            await client.query("insert into public.users (username, password, avatar) values($1, $2, $3)", [faker.internet.userName(), hash, faker.image.avatar()])
         }
 
         const res = await client.query("select id from public.users order by created_at desc limit $1", [numUsers])
