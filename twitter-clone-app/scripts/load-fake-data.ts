70f3043a1a654680e4ec284209197fdd0d06c1c6
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-04-24 00:17:54 -0400

    Fake data loading

--- twitter-clone-app/scripts/load-fake-data.ts
+++ twitter-clone-app/scripts/load-fake-data.ts
@@ -1,5 +1,6 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
+import {faker} from "@faker-js/faker"
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
@@ -14,15 +15,49 @@ async function loadFakeData(numUsers = 10) {
         password: process.env.POSTGRES_PASSWORD,
         port: parseInt(process.env.POSTGRES_PORT!)
     });
-
+    await client.connect();
     try {
-        await client.connect();
-        const res = await client.query("SELECT 1"); 
-        console.log("Query result:", res.rows);
+        await client.query("begin")
+
+        for (let i = 0; i < numUsers; i++){
+            await client.query("insert into public.users (username, password, avatar) values($1, $2, $3)", [faker.internet.userName(), "password", faker.image.avatar()])
+        }
+
+        const res = await client.query("select id from public.users order by created_at desc limit $1", [numUsers])
+        console.log(res.rows);
+
+        for (const row of res.rows){
+            for(let i=0; i<Math.ceil(Math.random()*10); i++){
+                await client.query(
+                    "insert into public.posts (user_id, content) values ($1, $2)", [row.id, faker.lorem.sentence()]
+                )
+
+            }
+        }
+
+        for(const row1 of res.rows){
+            for (const row2 of res.rows){
+                if (row1.id!==row2.id){
+                    if (Math.random()>0.5){
+                        await client .query("insert into follows (user_id , follower_id) values ($1, $2)", [row1.id, row2.id]);
+                    }
+                }
+            }
+        }
+
+
+
+        await client.query("commit")
+
     } catch (error) {
-        console.error("Error executing query:", error);
+        await client.query("rollback")
+        throw error
     }
     await client.end()
 }
 
-loadFakeData();
+const numUsers = parseInt(process.argv[2]) || 10;
+console.log(`loading ${numUsers} fakeusers`);
+loadFakeData(numUsers);
+
+
