7292bba935d4e5bfafadca1eab2bf92058f6f4ff
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-04-29 14:44:58 -0400

    added user avatar on layout

--- twitter-clone-app/app/(private)/header.tsx
+++ twitter-clone-app/app/(private)/header.tsx
@@ -1,23 +1,21 @@
 "use client";
 import useSWR from "swr";
+import User from "../components/user";
 
-export default function Header(){
-    const {data, error, isLoading} = useSWR("/api/users/profile")
-    if (error){
-        return (
-            <div>Failed to load</div>
-        )
-    }
-    if(isLoading){
-        return (
-            <div>loading....</div>
-        )
-    }
+export default function Header() {
+  const { data, error, isLoading } = useSWR("/api/users/profile");
 
-    console.log(data);
-    return(
-        <header>
-            {data.data.username}
-        </header>
-    )
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <header className="flex flex-row w-full p-5 dark:bg-slate-800 bg-slate-300 rounded-lg my-2 justify-between items-center">
+      <div>
+        <h1 className="font-mono text-lg">Twitter-Clone</h1>
+      </div>
+      <div>
+        <User user={data.data} href="account" />
+      </div>
+    </header>
+  );
 }
