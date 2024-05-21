d24867bdac8d438a8ba42fe1befcb0942cf048c0
RohithGummadi <rohithmc.gummadi@gmail.com>
2024-05-21 17:53:10 -0400

    added server component to see

---
+++ twitter-clone-app/app/(public)/public-feed/page.tsx
@@ -0,0 +1,24 @@
+import Post from "@/app/components/post";
+import { sql } from "@/db";
+
+async function getData(){
+    const res = await sql(`select p.*, u.username from posts p inner join users u on p.user_id = u.id order by created_at desc limit 10`)
+    return res.rows;
+}
+
+export default async function PublicFeed(){
+    const posts = await getData();
+    return (
+        <main>
+            <div className="flex flex-row bg-orange-400 m-4 p-3" >
+                <h1>Twitter Clone -  </h1>
+                <h2>Recent posts from the community</h2>
+            </div>
+            <div>
+                {posts.map((post)=>{
+                    return <Post post={post}/>
+                })}
+            </div>
+        </main>
+    )
+}
